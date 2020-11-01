6ce19932eecc32e0042f3c73d22ca31bc609494c
AnggaPermana2 <71532932+AnggaPermana2@users.noreply.github.com>
2020-11-02 00:07:36 +0800

    Update tampil.php

--- admin/views/dokter/tampil.php
+++ admin/views/dokter/tampil.php
@@ -1,55 +1,37 @@
-<h4>Tambah Data</h4>
-<hr>
-<form action="index.php?mod=dokter&page=save" method="POST" enctype="multipart/formdata">
-    <div class="col-md-6">
-        <div class="form-group">
-            <label for="">Nama Dokter</label>
-            <input type="text" name="nama_dokter" required value="<?=(isset($_POST['nama_dokter']))?$_POST['nama_dokter']:'';?>" class="form-control">
-            <input type="hidden" name="id_dokter"  value="<?=(isset($_POST['id_dokter']))?$_POST['id_dokter']:'';?>" class="form-control">
-            <span class="text-danger"><?=(isset($err['nama_dokter']))?$err['nama_dokter']:'';?></span>
-        </div>
-        <div class="form-group">
-        <label for="">No IDI</label>
-            <input type="number" name="no_id" value="<?=(isset($_POST['no_id']))?$_POST['no_id']:'';?>" class="form-control">
-            <span class="text-danger"><?=(isset($err['no_id']))?$err['no_id']:'';?></span>
-        </div>
-        <div class="form-group">
-        <label for="">NIP</label>
-            <input type="number" name="nip" value="<?=(isset($_POST['nip']))?$_POST['nip']:'';?>" class="form-control">
-            <span class="text-danger"><?=(isset($err['nip']))?$err['nip']:'';?></span>
-        </div>
+<div class="row">
+    <div class="pull-left">
+        <h4>Daftar Dokter</h4>
     </div>
-    <div class="col-md-6">
-    <div class="form-group">
-            <label for="">Pendidikan Terakhir</label>
-            <select name="id_pendidikan" class="form-control" required id="" >
-            <option value="">Pilih Spesialisasi</option>
-                <?php if($pendidikan != NULL){
-                    foreach($pendidikan as $row){?>
-                        <option <?=(isset($_POST['id_pendidikan']) && $_POST['id_pendidikan']==$row['id_pendidikan'] )?"selected":'';?> value="<?=$row['id_pendidikan'];?>"> <?=$row['pendidikan'];?></option>
-                    <?php }
-                }?>
-            </select>
-            <span class="text-danger"><?=(isset($err['id_pendidikan']))?$err['id_pendidikan']:'';?></span>
+    <div class="pull-right">
+        <a href="index.php?mod=dokter&page=add">
+            <button class="btn btn-primary">Add</button>
+        </a>
     </div>
-    <div class="form-group">
-            <label for="">Spesialisasi</label>
-            <select name="id_spesialisasi" class="form-control" required id="">
-                <option value="">Pilih Spesialisasi</option>
-                <?php if($spesialis != NULL){
-                    foreach($spesialis as $row){?>
-                        <option <?=(isset($_POST['id_spesialisasi']) && $_POST['id_spesialisasi']==$row['id_spesialisasi'] )?"selected":'';?> value="<?=$row['id_spesialisasi'];?>"> <?=$row['nama_spesialisasi'];?></option>
-                    <?php }
-                }?>
-            </select>
-            <span class="text-danger"><?=(isset($err['id_spesialisasi']))?$err['id_spesialisasi']:'';?></span>
-    </div>
-    <div class="form-group">
-    <label for="Photos"></label>
-    <input type="file" name="fileupload" class="form-control">
-    <span class="text-danger"><?=(isset($err['fileupload']))?$err['fileupload']:'';?></span>
-    <div class="form-group">
-            <button type="submit" class="btn btn-primary">Save</button>
-    </div>
-    </div>
-</form>
+</div>
+<div class="row">
+    <table class="table">
+        <thead>
+            <tr>
+                <td>
+                    #
+                </td>
+                <td>Nama</td><td>ID</td><td>NIK</td><th>Pendidikan</th><th>Spesialis</th><td>Aksi</td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if($dokter != NULL){ 
+                $no=1;
+                foreach($dokter as $row){?>
+                    <tr>
+                        <td><?=$no;?></td><td><?=$row['nama_dokter']?></td><td><?=$row['no_peserta_idi'];?></td><td><?=$row['nik']?></td>
+                        <td><?=$row['pendidikan']?></td><td><?=$row['nama_spesialisasi']?></td>                        
+                        <td>
+                            <a href="index.php?mod=dokter&page=edit&id=<?=md5($row['id_dokter'])?>"><i class="fa fa-pencil"></i> </a>
+                            <a href="index.php?mod=dokter&page=delete&id=<?=md5($row['id_dokter'])?>"><i class="fa fa-trash"></i> </a>
+                        </td>
+                    </tr>
+               <?php $no++; }
+            }?>
+        </tbody>
+    </table>
+</div>
