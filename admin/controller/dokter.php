f5569191de983f7ae276abb1057276150aeff7c5
AnggaPermana2 <71532932+AnggaPermana2@users.noreply.github.com>
2020-11-02 00:05:26 +0800

    Update dokter.php

--- admin/controller/dokter.php
+++ admin/controller/dokter.php
@@ -1,55 +1,45 @@
-<h4>Tambah Data</h4>
-<hr>
-<form action="index.php?mod=dokter&page=save" method="POST" enctype="multipart/formdata">
-    <div class="col-md-6">
-        <div class="form-group">
-            <label for="">Nama Dokter</label>
-            <input type="text" name="nama_dokter" required value="<?=(isset($_POST['nama_dokter']))?$_POST['nama_dokter']:'';?>" class="form-control">
-            <input type="hidden" name="id_dokter"  value="<?=(isset($_POST['id_dokter']))?$_POST['id_dokter']:'';?>" class="form-control">
-            <span class="text-danger"><?=(isset($err['nama_dokter']))?$err['nama_dokter']:'';?></span>
-        </div>
-        <div class="form-group">
-        <label for="">No IDI</label>
-            <input type="number" name="no_id" value="<?=(isset($_POST['no_id']))?$_POST['no_id']:'';?>" class="form-control">
-            <span class="text-danger"><?=(isset($err['no_id']))?$err['no_id']:'';?></span>
-        </div>
-        <div class="form-group">
-        <label for="">NIP</label>
-            <input type="number" name="nip" value="<?=(isset($_POST['nip']))?$_POST['nip']:'';?>" class="form-control">
-            <span class="text-danger"><?=(isset($err['nip']))?$err['nip']:'';?></span>
-        </div>
-    </div>
-    <div class="col-md-6">
-    <div class="form-group">
-            <label for="">Pendidikan Terakhir</label>
-            <select name="id_pendidikan" class="form-control" required id="" >
-            <option value="">Pilih Spesialisasi</option>
-                <?php if($pendidikan != NULL){
-                    foreach($pendidikan as $row){?>
-                        <option <?=(isset($_POST['id_pendidikan']) && $_POST['id_pendidikan']==$row['id_pendidikan'] )?"selected":'';?> value="<?=$row['id_pendidikan'];?>"> <?=$row['pendidikan'];?></option>
-                    <?php }
-                }?>
-            </select>
-            <span class="text-danger"><?=(isset($err['id_pendidikan']))?$err['id_pendidikan']:'';?></span>
-    </div>
-    <div class="form-group">
-            <label for="">Spesialisasi</label>
-            <select name="id_spesialisasi" class="form-control" required id="">
-                <option value="">Pilih Spesialisasi</option>
-                <?php if($spesialis != NULL){
-                    foreach($spesialis as $row){?>
-                        <option <?=(isset($_POST['id_spesialisasi']) && $_POST['id_spesialisasi']==$row['id_spesialisasi'] )?"selected":'';?> value="<?=$row['id_spesialisasi'];?>"> <?=$row['nama_spesialisasi'];?></option>
-                    <?php }
-                }?>
-            </select>
-            <span class="text-danger"><?=(isset($err['id_spesialisasi']))?$err['id_spesialisasi']:'';?></span>
-    </div>
-    <div class="form-group">
-    <label for="Photos"></label>
-    <input type="file" name="fileupload" class="form-control">
-    <span class="text-danger"><?=(isset($err['fileupload']))?$err['fileupload']:'';?></span>
-    <div class="form-group">
-            <button type="submit" class="btn btn-primary">Save</button>
-    </div>
-    </div>
-</form>
+<?php
+    $con->auth();
+    $con=$con->koneksi
+    switch (#$_GET['page]){
+    case 'add';
+        $pendidikan="select * from pendidikan";
+        $pendidikan=$conn->query($pendidikan);
+        $sql="select * from spesialis";
+        $spesialis=$conn->query($sql)
+        $content="views/dokter/tambah.php";
+        include_once 'views/template/php';
+    break;
+        case 'save';
+        if($_SERVER['REQUEST_METHOD']="POST"){
+            $id_pegawai=$_SESSION['login']['id'];
+            $sql = "INSERT INFO dokter (nama_dokter, no_peserta, id_spesialis, id_pegawai")
+            VALUES ($_POST[nama_dokter]','$_POST[no_id]','$_POST[nip]','$_POST[id_spesialis]','$_SESSION[login][id]);
+            if ($conn->querry($sql) == TRUE) {
+                header('location : '.$con->site_url().'/admin/index.php?mode=dokter');
+                else {
+                    echo "error: " . $sql . "<br>" . $conn=>error;
+                }
+            }
+        }
+        else{
+            $err['msg']="tidak diijinkan";
+        }
+        if(isset($error)){
+        $pendidikan="select * from pendidikan";
+        $pendidikan=$conn->query($pendidikan);
+        $sql="select * from spesialis";
+        $spesialis=$conn->query($sql)
+        $content="views/dokter/tambah.php";
+        include_once 'views/template/php';
+        }
+break;
+    case 'edit';
+    echo 'edit';
+default;
+    $sql ="select * from dokter";
+    $dokter=$conn->querry($sql);
+    $conn->close();
+    $content="views/dokter/tampil.php";
+    include_once 'views/template.php';
+?>
